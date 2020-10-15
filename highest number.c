5a11f647cab1bdc817d0b545b6d9f3e037cbec01
Swakshwar-Ghosh <ghoshswakshwar@gmail.com>
2020-10-15 22:26:10 +0530

    Update highest number.c

--- highest number.c
+++ highest number.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 int main()
 {
-	int a,b,c;
+	int a,b,c,d;
 	printf("\n enter first number here");
 	scanf("%d",&a);
 	printf("\n enter second number here");
 	scanf("%d",&b);
 	printf("\n enter third number here");
 	scanf("%d",&c);
-	if(a>b && a>c)
-	printf("\n %d is the highest number",a);
-    if(b>a && b>c)
-	printf("\n %d is the highest number",b);
+	if(a>=b && a>=c)
+	{
+	    d=a;
+	}
+    if(b>=a && b>=c)
+	{
+	    d=b;
+	}
 
-    if(c>a && c>b)
-	printf("\n %d is the highest number",c);
-    else if (a==b && b==c && a==c)
-	printf("\n all numbers are equal");
-	else if (a==b && a>c)
-        printf("\n %d is the highest number",a);
-    else if(b==c && b>a)
-        printf("\n %d is the highest number",b);
-    else if(c==a && c>b)
-        printf("\n %d is the highest number",c);
+    if(c>=a && c>=b)
+	{
+	    d=c;
+	}
+
+	printf("\n the highest number is %d",d);
+
 	return 0;
 
 }
